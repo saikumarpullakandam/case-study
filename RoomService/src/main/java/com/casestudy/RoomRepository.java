f777c9fab29e45f42b71053a70738eb8488ed27b
saikumar pullakandam <92907352+saikumarpullakandam@users.noreply.github.com>
2021-12-23 22:02:49 +0530

    Delete RoomService directory

--- RoomService/src/main/java/com/casestudy/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.casestudy;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends MongoRepository<Room,String>{
-
-}
