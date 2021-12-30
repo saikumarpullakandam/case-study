dcaa63be427b11e6dec39af9467a409646574d66
saikumar pullakandam <92907352+saikumarpullakandam@users.noreply.github.com>
2021-12-31 00:28:09 +0530

    Add files via upload

--- Room-service/src/main/java/com/Roomservice/Controllers/RoomController.java
+++ Room-service/src/main/java/com/Roomservice/Controllers/RoomController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.ws.rs.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,42 +22,41 @@ import com.Roomservice.Services.ManagerService;
 
 
 @RestController
+@CrossOrigin ("http://localhost:4200")
 @RequestMapping("/Room")
+
 public class RoomController {
 	
 	@Autowired
 	private ManagerService service;
 	
 	
-//	@GetMapping("/hello")
-//	public String hello() {
-//		return "Hello manager";
-//	}
-	
+
+	@CrossOrigin ("http://localhost:4200")
 	@PostMapping("/addRoom")
 	public Room addRoom(@RequestBody Room room) {
 		System.out.println(room.getRoomNum());
 		return this.service.addRoom(room);
 	}
-	
+	@CrossOrigin
 	@GetMapping("/getRoom/{id}")
 	public Optional<Room> getRoom(@PathVariable("id") String id)
 	{
 		return this.service.getRoom(Integer.parseInt(id));
 	}
-	
+	@CrossOrigin
 	@PutMapping("/updateRoom")
 	public Room updateRoom(@RequestBody Room room)
 	{
 		return this.service.updateRoom(room);
 	}
-	
+	@CrossOrigin
 	@DeleteMapping("/deleteRoom/{id}")
 	public String deleteRoom(@PathVariable("id") String id)
 	{
 		return this.service.deleteRoom(Integer.parseInt(id));
 	}
-	
+	@CrossOrigin ("http://localhost:4200")
 	@GetMapping("/getAllRooms")
 	public RoomList getAllGuest()
 	{
@@ -64,7 +64,7 @@ public class RoomController {
 		list.setAllRooms(this.service.getAllRooms());
 		return list;
 	}
-	
+	@CrossOrigin
 	@GetMapping("/getAvlRooms")
 	public RoomList getAvlRooms()
 	{
