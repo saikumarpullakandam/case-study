b6a87bce03e672cf2e98978b536cb9c438503cbc
saikumar pullakandam <92907352+saikumarpullakandam@users.noreply.github.com>
2022-01-01 15:24:29 +0530

    Add files via upload

--- apigateway/src/main/java/com/practice/ApigatewayApplication.java
+++ apigateway/src/main/java/com/practice/ApigatewayApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
 public class ApigatewayApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
